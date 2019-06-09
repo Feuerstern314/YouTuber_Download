79418907e03aa83ddc6c3a558015daff88eb0cfd
Adrian Bloober <adrianbloober@gmail.com>
2019-06-09 19:31:17 +0200

    MP3 Support and playlists and videos support

--- __main__.py
+++ __main__.py
@@ -12,7 +12,41 @@ def get_infos_about_video(video) -> str:
     duration = video.duration
 
     return f'Title: \'{title}\'  Author: \'{author}\'  Duration: \'{duration}\''
-def setup_playlist():
+
+
+def iterate_playlist(out_type):
+    urls = input('URLS(;): ').split(';')
+
+    for playlist in urls:
+        print('Infos: ' + get_infos_about_playlist(playlist))
+        items = playlist['items']
+        print(f'Length: {len(items)}')
+        os.mkdir(playlist['title'])
+        l = len(items)
+        print(f'0%')
+        for i in range(len(items)):
+            item = items[i - 1]['pafy']
+            if out_type.lower() == 'mp4':
+                download = item.getbest().download(filepath=playlist['title'])
+            if out_type.lower() == 'mp3':
+                download = item.getbestaudio().download(filepath=playlist['title'])
+            r = i
+            procent = r / l
+            procent = round(procent, 2)
+            print(f'{procent * 100}%')
+    print('Finished')
+def iterate_video(out_type):
+    videos = input('URLS(;): ').split(';')
+
+    for video_raw in videos:
+        video = pafy.new(video_raw)
+        print('Infos: ' + get_infos_about_video(video))
+        if out_type.lower() == 'mp3':
+            download = video.getbestaudio().download()
+        if out_type.lower() == 'mp4':
+            download = video.getbest().download()
+    print('Finished')
+def setup_playlist(out_type):
     playlist = pafy.get_playlist(input('URL: '))
 
     print('Infos: ' + get_infos_about_playlist(playlist))
@@ -34,7 +68,10 @@ def setup_playlist():
         print(f'0%')
         for i in range(len(items)):
             item = items[i - 1]['pafy']
-            download = item.getbest().download(filepath=playlist['title'])
+            if out_type.lower() == 'mp4':
+                download = item.getbest().download(filepath=playlist['title'])
+            if out_type.lower() == 'mp3':
+                download = item.getbestaudio().download(filepath=playlist['title'])
             r = i
             procent = r / l
             procent = round(procent, 2)
@@ -42,19 +79,23 @@ def setup_playlist():
         print('Finished')
         return
     return
-def setup_video():
+def setup_video(out_type):
     video =  pafy.new(input('URL: '))
 
     print('Infos: ' + get_infos_about_video(video))
-    i = input('Donwload? y/n')
+    i = input('Download? y/n')
     if i.lower() == 'y' or i.lower() == 'j':
-        download = video.getbest().download()
+        if out_type.lower() == 'mp3':
+            download = video.getbestaudio().download()
+        if out_type.lower() == 'mp4':
+            download = video.getbest().download()
         print('Finished')
     return
 t = input('Type: ')
-
+output = input('Output mp4/mp3 ')
 if t.lower() == 'playlist':
-    setup_playlist()
+    setup_playlist(output)
+elif t.lower() == 'playlists':
+    iterate_playlist(output)
 else:
-    setup_video()
-#END
+    setup_video(output)
